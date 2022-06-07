2f15be83a36c020c54c55805a5dc3b8d40fefd31
Zahidul Islam Suzon <zahidul.islam@welldev.io>
2022-06-07 12:22:07 +0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,44 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://media.united.com/images/Media%20Database/SDL/gltw/logos-united-logo-rebrand-large.png"
+  },
+])
+
+reviews = Review.create([
+  {
+    title: "Great Airline",
+    description: "I had good time",
+    score: "5",
+    airline: airlines.first
+  },
+  {
+    title: "Bad Airline",
+    description: "I had bad time",
+    score: "1",
+    airline: airlines.first
+  },
+])
